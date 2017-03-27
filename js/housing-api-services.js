d9ab83d6f43f79585a275fb77727e1a9babc39a5
woaij007 <Cola@Juns-Mac-mini.lan>
2017-03-27 15:36:11 -0700

    updated client sdk 2.0

--- js/housing-api-services.js
+++ js/housing-api-services.js
@@ -10791,6 +10791,49 @@ if (typeof module !== 'undefined' && typeof exports !== 'undefined' &&
 
             /**
              * @ngdoc method
+             * @name housingApiServices.Apartment#sendEmail
+             * @methodOf housingApiServices.Apartment
+             *
+             * @description
+             *
+             * <em>
+             * (The remote method definition does not provide any description.)
+             * </em>
+             *
+             * @param {Object=} parameters Request parameters.
+             *
+             *   This method does not accept any parameters.
+             *   Supply an empty object or omit this argument altogether.
+             *
+             * @param {Object} postData Request data.
+             *
+             *  - `to` – `{*}` - the emails to send to
+             *
+             *  - `apartmentName` – `{string}` - the name of apartment
+             *
+             *  - `userId` – `{string}` - the id of user
+             *
+             *  - `res` – `{object=}` -
+             *
+             * @param {function(Object,Object)=} successCb
+             *   Success callback with two arguments: `value`, `responseHeaders`.
+             *
+             * @param {function(Object)=} errorCb Error callback with one argument:
+             *   `httpResponse`.
+             *
+             * @returns {Object} An empty reference that will be
+             *   populated with the actual data once the response is returned
+             *   from the server.
+             *
+             * This method returns no data.
+             */
+            "sendEmail": {
+              url: urlBase + "/apartments/sendEmail",
+              method: "POST",
+            },
+
+            /**
+             * @ngdoc method
              * @name housingApiServices.Apartment#coupon
              * @methodOf housingApiServices.Apartment
              *
